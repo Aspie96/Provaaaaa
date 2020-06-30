d4af522333dfdf8c202d141d1a17a3d435620738
Valentino Giudice <valentino.giudice96@gmail.com>
2020-06-30 23:33:33 +0200

    First commit

---
+++ src/main/java/com/example/Provaaaaa/NewClass.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.Provaaaaa;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+class NewClass {
+	@GetMapping
+        @ResponseBody
+	public String home() {
+		return "newhtml";
+	}
+}
---
+++ src/main/java/com/example/Provaaaaa/ProvaaaaaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Provaaaaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaaaaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaaaaaApplication.class, args);
+	}
+
+}
