0313285569e6119b5bd4b11e19a2574919fde1e4
Valentino Giudice <valentino.giudice96@gmail.com>
2020-06-30 23:49:25 +0200

    AaHAIJHHHH

---
+++ src/main/java/com/example/Provaaaaa/UserDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.Provaaaaa;
+
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Aspie96
+ */
+public interface UserDao extends CrudRepository<UserE, Integer> {
+    public Optional<UserE> findByUsername(String username);
+}
